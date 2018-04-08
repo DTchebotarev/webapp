126a1844da7217002e178dc140b60e2d188b015b
Dmitri Tchebotarev <dmitri.tchebotarev@gmail.com>
2018-04-08 17:58:51 -0400

    Try players

--- hello.py
+++ hello.py
@@ -2,6 +2,15 @@ from flask import Flask
 import os
 app = Flask(__name__)
 
+player_list = set()
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/add_player/<player_id>')
+def add_player(player_id):
+    player_list.add(player_id)
+
+@app.route('list_ids'):
+    return "\n".join(player_id)
