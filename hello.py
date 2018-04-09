bc2c54e5b5c8eb7bc909f95cb01ab9d81b5a33c3
DTchebotarev <DTchebotarev@users.noreply.github.com>
2018-04-09 17:59:06 -0400

    Update hello.py
    Shrink?

--- hello.py
+++ hello.py
@@ -88,7 +88,7 @@ current_player_id = 0
 
 common_head = '''<!doctype html><html lang-"en"><head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,shrink-to-fit=no">
     {}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head><body>'''
