59e557917a9e7da8e2999e4431b9feb671374dfe
Dmitri Tchebotarev <DTchebotarev@users.noreply.github.com>
2018-04-09 22:19:41 -0400

    Mon Apr  9 22:19:41 EDT 2018

--- hello.py
+++ hello.py
@@ -101,7 +101,7 @@ common_head_cpi = '''<!doctype html><html lang="en"><head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0,shrink-to-fit=no">
     {}
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     </head><body>
     <div data-role="page">
