53eb7d5f6909d9bae3405239f284c12c56ad4c2f
Dmitri Tchebotarev <DTchebotarev@users.noreply.github.com>
2018-04-09 21:04:23 -0400

    Mon Apr  9 21:04:23 EDT 2018

--- hello.py
+++ hello.py
@@ -90,8 +90,10 @@ common_head = '''<!doctype html><html lang-"en"><head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,shrink-to-fit=no">
     {}
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    </head><body>'''
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    </head><body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    '''
 common_tail = '''<br><a href='/'>Home</a></body></html>'''
 def roster_expected_goals(roster, nsim=1000):
     # add expected goals
